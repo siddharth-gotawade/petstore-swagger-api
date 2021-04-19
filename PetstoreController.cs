05f63d7174dfc4b9179ac61dc6c6a6a04d5a3cde
siddharth-gotawade <82651347+siddharth-gotawade@users.noreply.github.com>
2021-04-19 16:26:48 +0530

    Create PetstorController.cs

---
+++ PetstoreController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Petstore_Swagger_Api.Controllers
+{
+    public class PetstoreController : Controller
+    {
+        // POST api/pet/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        //POST api/pet/{petId}/uploadImage
+        public void Post(int petId, [FromBody]string value)
+        {
+        }
+
+        // PUT api/pet/values/5
+        public void Put(int petId, [FromBody]string value)
+        {
+        }
+
+
+        // GET api/pet/status/5
+        public string Get(string status)
+        {
+            return "value";
+        }
+
+        // GET api/pet/values/{petid}
+        public string Get(int? petId)
+        {
+            return "value";
+        }
+
+        // DELETE api/pet/values/{petid}
+        public void Delete(int? petId)
+        {
+        }
+
+    }
+}
